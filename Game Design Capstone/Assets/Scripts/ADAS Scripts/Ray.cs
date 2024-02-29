f15e96d1769dff7abd68559b11689151ddbd8d0e
Dante <notapirate69yarhar@gmail.com>
2024-02-29 07:58:38 -0800

    switch to unity china
    we need to switch to unity china

--- Game Design Capstone/Assets/Scripts/ADAS Scripts/Ray.cs
+++ Game Design Capstone/Assets/Scripts/ADAS Scripts/Ray.cs
@@ -8,9 +8,9 @@ public class Ray : MonoBehaviour
      public LayerMask layerMask; // Layers to consider for the raycast
      public Vector3 rayDirection;
      public float drawDistance = 5.0f;
-     public float interpolationRatio = 0.5f;
+     public float interpolationRatio = 1.0f;
      public float lifeSpan = 1.0f;
-     public float stepDensity = 0.5f;
+     public float stepDensity = 0.05f;
 
      void Update()
      {
@@ -24,24 +24,25 @@ public class Ray : MonoBehaviour
           Vector3 rayDirection = transform.TransformDirection(Vector3.forward);
 
           
+          
           for (float x = 0.0f; x < interpolationRatio; x += stepDensity)
           {
                for (float y = 0.0f; y < interpolationRatio; y += stepDensity)
                {
-                    //float xGrad;
+                    //Vector3 horizontalVec = Vector3(0.0f, 0.0f, (transform.right.z - (transform.right + stepDensity) * x )).normalize;
                     Debug.DrawLine(transform.position, transform.position + (transform.forward + Vector3.Lerp(-transform.up, transform.up, y) + Vector3.Lerp(-transform.right, transform.right, x)) * drawDistance, Color.blue, lifeSpan, false);
-                    Debug.DrawLine(transform.position, transform.position + (transform.forward + Vector3.Lerp(-transform.up, transform.up, y) + Vector3.Lerp(-transform.right, transform.right, x)) * drawDistance, Color.blue, lifeSpan, false);
+                    //Debug.DrawLine(transform.position, transform.position + (transform.forward + Vector3.Lerp(-transform.up, transform.up, y) + Vector3.Lerp(-transform.right, transform.right, x)) * drawDistance, Color.blue, lifeSpan, false);
                }
           }
 
-          Debug.Log(transform.position);
+          //Debug.Log(transform.position);
 
-          Debug.DrawLine(transform.position, transform.position + transform.forward * drawDistance, Color.blue, lifeSpan, false);
+          //Debug.DrawLine(transform.position, transform.position + transform.forward * drawDistance, Color.blue, lifeSpan, false);
 
 
-          Debug.DrawLine(transform.position, transform.position + Vector3.Lerp(transform.forward, transform.up, interpolationRatio) * drawDistance, Color.blue, lifeSpan, false);
-          Debug.DrawLine(transform.position, transform.position + Vector3.Lerp(transform.forward, (transform.up+transform.right), interpolationRatio) * drawDistance, Color.blue, lifeSpan, false);
-          Debug.DrawLine(transform.position, transform.position + Vector3.Lerp(transform.forward, transform.right, interpolationRatio) * drawDistance, Color.blue, lifeSpan, false);
+          //Debug.DrawLine(transform.position, transform.position + Vector3.Lerp(transform.forward, transform.up, interpolationRatio) * drawDistance, Color.blue, lifeSpan, false);
+         //Debug.DrawLine(transform.position, transform.position + Vector3.Lerp(transform.forward, (transform.up+transform.right), interpolationRatio) * drawDistance, Color.blue, lifeSpan, false);
+          //Debug.DrawLine(transform.position, transform.position + Vector3.Lerp(transform.forward, transform.right, interpolationRatio) * drawDistance, Color.blue, lifeSpan, false);
           // up
           //Debug.DrawLine(transform.position, transform.position + (Vector3(transform.forward.x, transform.forward.y + 0.25f, transform.forward.z) * drawDistance), Color.blue, 10.0f, false);
 
