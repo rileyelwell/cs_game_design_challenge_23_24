f2e23a69e5ee74c0f1fc1b2f20148d881b9a66b3
Ethan Largent <largenet@oregonstate.edu>
2024-04-24 10:35:37 -0700

    Walking pedestrians
    Pedestrian model was changed and are animated with a walking motion. A commented out attempt at getting randomized spawn locations in PedestrianPathEditor

--- Game Design Capstone/Assets/Scripts/AI Pathfinding Scripts/PedestrianPathing/PedestrianPathEditor.cs
+++ Game Design Capstone/Assets/Scripts/AI Pathfinding Scripts/PedestrianPathing/PedestrianPathEditor.cs
@@ -1,5 +1,9 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PedestrianPathEditor : MonoBehaviour
@@ -7,6 +11,68 @@ public class PedestrianPathEditor : MonoBehaviour
     public Color rayColor = Color.white;
     public List<Transform> path_objs = new List<Transform> ();
     Transform[] theArray;
+    
+    public GameObject pedestrian;
+    public float minDensity;
+    public float maxDensity;
+    // Randomized Spawning
+    /*
+    void Start()
+    {
+        theArray = GetComponentsInChildren<Transform>();
+        float pathLength = 0;
+        for(int i = 0; i < theArray.Length - 1; i++)
+        {
+            pathLength += Vector3.Distance(theArray[i].position, theArray[i+1].position);
+        }
+        pathLength += Vector3.Distance(theArray[theArray.Length - 2].position, theArray[theArray.Length - 1].position);
+        UnityEngine.Debug.Log("Path Length:" + pathLength);
+        float currentLength = 0;
+        float incrementLength;
+        float distanceFromNext;
+
+        int currentIndex = 0;
+        int nextIndex = 1;
+
+        Transform currentPosition = theArray[currentIndex];
+        float incrementX;
+        float incrementZ;
+        while(currentLength < pathLength && currentIndex < theArray.Length)
+        {
+            UnityEngine.Debug.Log("Spawned!");
+            Instantiate(pedestrian, currentPosition);
+            incrementLength = UnityEngine.Random.Range(minDensity, maxDensity);
+            distanceFromNext = Vector3.Distance(theArray[currentIndex].position, theArray[nextIndex].position);
+            while ( incrementLength > distanceFromNext && currentIndex < theArray.Length)
+            {
+                incrementLength -= distanceFromNext;
+                currentIndex++;
+                if (currentIndex < theArray.Length - 1)
+                {
+                    nextIndex++;
+                }
+                else if (currentIndex == theArray.Length - 1)
+                {
+                    nextIndex = 0;
+                }
+                else
+                {
+                    break;
+                }
+                distanceFromNext = Vector3.Distance(theArray[currentIndex].position, theArray[nextIndex].position);
+            }
+            if(currentIndex == theArray.Length)
+            {
+                break;
+            }
+            currentPosition = theArray[currentIndex];
+            incrementX = incrementLength / distanceFromNext * (theArray[nextIndex].position.x - currentPosition.position.x);
+            incrementZ = incrementLength / distanceFromNext * (theArray[nextIndex].position.z - currentPosition.position.z);
+            currentPosition.position += new Vector3(incrementX, 0, incrementZ);
+        }
+
+
+    }*/
 
     void OnDrawGizmos()
     {
